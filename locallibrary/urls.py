abb1c59e06a58cd23397165b3e9d0601d49bbee4
Akifumi Maeda <akifumi_maeda@maede.co.jp>
2021-08-10 10:58:06 +0900

    add the link for admin site's url

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib import auth
 from django.urls import path
 
 # Use include() to add paths from the catalog application
